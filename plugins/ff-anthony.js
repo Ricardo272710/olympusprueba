6d12834b8705fed9a2dfc6549a17ef8ea43fdb2f
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-08-01 22:43:27 -0500

    Create ff-anthony.js

---
+++ plugins/ff-anthony.js
@@ -0,0 +1,16 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+hola bebes
+` 
+}
+handler.command = /^(anthp|anthony)$/i
+handler.register = true
+handler.group = true
+handler.admin = true
+export default handler
